747be512a2abdb3cc7ba5f8ad3883c2ef8986f7c
Соколов Александр Александрович <Sokolov_aa@office.client.loc>
2024-09-05 17:13:39 +0300

    redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,45 @@
-import { createStore } from 'redux';
-import reducer from '../reducers';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
+import { createSelector } from 'reselect';
+import { type } from '@testing-library/user-event/dist/cjs/utility/type.js';
+import { act } from 'react';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleWare = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+// const enhencer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action)
+//     }
+//     return store;
+// }
+
+const store = createStore(
+    combineReducers({ heroes, filters }),
+    compose(applyMiddleware(thunk, stringMiddleWare),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    // compose(
+    //     enhencer,
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )
+);
 
 export default store;
+
